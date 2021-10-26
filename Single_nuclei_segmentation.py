41a2bc54725ea2aad87e3d98e32212ad4bb4b7b1
Jacob-Relle <81320137+Jacob-Relle@users.noreply.github.com>
2021-10-26 16:29:26 +0200

    Schritte 1 und zwei implementiert
    Kleine Änderung in Alg 2 betrachte pixel anzahl für c nicht anzahl an fragmenten.

---
+++ Single_nuclei_segmentation.py
@@ -0,0 +1,76 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.ndimage.filters import gaussian_filter
+
+from skimage import filters
+
+from cvxopt import solvers, matrix, div, exp, mul, log
+
+def J_energy(image):
+
+    #The coordinates of the image
+    coords = [(x[0], x[1]) for x in np.ndindex(image.shape)]
+    #define tau
+    tau = filters.threshold_otsu(image)
+    #define y_x for every element in the region
+    y = matrix(np.array([image[x[0], x[1]] - tau for x in coords],dtype=np.double),(len(coords),1))
+    #define nable s(x) for each element
+    delta_s = matrix(np.array([[x[0]**2, x[1]**2, 2*x[0]*x[1], x[0], x[1], 1] for x in coords]), (len(coords), 6))
+    def F(theta = None, z = None):
+        # choose starting point and as such define dimension of theta
+        if theta is None: return 0, matrix(0.0, (6,1))
+        # s contains region.area many rows, each row contains a scalar.
+        s = delta_s * theta
+        phi = log(1 + exp(-mul(y,s)))
+        J = sum(phi)
+        kappa = div(1, 1+exp(mul(y,s)))
+        DJ = matrix(1, (1, len(coords))) * (-mul(mul(y, kappa) * matrix(1, (1, 6)), delta_s))
+        if z is None: return J, DJ
+        nu = mul(y**2, kappa - kappa**2)
+        #define matrix to multiply from the right for summation
+        SumMatrix = matrix(np.tile([[1,0,0,0,0,0], 
+                                    [0,1,0,0,0,0], 
+                                    [0,0,1,0,0,0], 
+                                    [0,0,0,1,0,0], 
+                                    [0,0,0,0,1,0], 
+                                    [0,0,0,0,0,1]], len(coords)))
+        eta = matrix([delta_s[i,:].T * delta_s[i,:] * nu[i] for i in range(len(coords))])
+        H = z[0]* SumMatrix * eta
+        return J, DJ, H
+    solv = solvers.cp(F)
+    return solv['x']
+
+def Solv(image):
+    #Set solver options
+    solvers.options['show_progress'] = False
+    #Initalize model parameter theta
+    theta = matrix(np.zeros((6,1)),(6,1))
+    #try to solve minimizing Problem of Energie J with high accuracy
+    try:
+        solvers.options['feastol'] = 1e-7
+        theta = J_energy(image)
+    except:
+        solvers.options['feastol'] = 1e-2
+        theta = J_energy(image)
+    return theta
+
+def segmented(image, theta,threshold):
+    coords = [(x[0], x[1]) for x in np.ndindex(image.shape)]
+    delta_s = matrix(np.array([[x[0]**2, x[1]**2, 2*x[0]*x[1], x[0], x[1], 1] for x in coords]),(len(coords),6))
+    s = delta_s * theta
+    s = np.reshape(s,image.shape)
+    image[abs(s)<threshold] = 1
+    return image
+
+def main(path_to_data):
+
+    image = plt.imread(path_to_data)[...,0]
+    theta = Solv(image)
+    segmentation = segmented(image,theta,20)
+    plt.imshow(segmentation)
+    plt.colorbar()
+    plt.show()
+    
+if __name__ == '__main__' :
+    path = 'dna-0.png'
+    main(path)
---
+++ generate_fragments.py
@@ -0,0 +1,84 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import itertools
+import networkx as nx 
+
+from skimage import img_as_ubyte, util, filters
+from scipy.ndimage import gaussian_filter
+from skimage.segmentation import watershed
+from skimage.morphology import disk
+from skimage.measure import label, regionprops, euler_number
+from skimage.future.graph import RAG
+
+from tqdm import tqdm
+
+def generate_fragments(img,std_dev,int_threshold,min_seed_dist,max_search_depth,max_frag_dist, background = 0):
+    img = img_as_ubyte(img)
+    #smooth image with gausian filter
+    g = gaussian_filter(img,std_dev)
+    #flatten the lower background percentile of the gaussian filter to 0 (for background)
+    g[g < np.percentile(g, background)] = 0
+    
+    #Fehlerabfangen
+    if std_dev < 0:
+        raise ValueError("smoothing strength needs to be positive")
+    if int_threshold > 1 or int_threshold < 0:
+        raise ValueError("relative intensity threshold needs to be between 0 and 1")
+    if min_seed_dist < 1:
+        raise ValueError("min seed distance needs to be >= 1")
+    if max_search_depth < 0:
+        raise ValueError("maximum search error needs to be positive")
+    if max_frag_dist < 0:
+        raise ValueError("maximum fragments distance needs zo be positive")
+    if background > 100 or background < 0:
+        raise ValueError('background is a percentage value between 0 and 100')
+        
+    #Create Delta Ball
+    B = disk(min_seed_dist)
+    #Set the markers in the Image according to the formula (P is an image)
+    P = (g==filters.rank.maximum(g,B)) & ((1-int_threshold)*g >= filters.rank.minimum(g,B))
+    #Fuse adjacient markers and label each group of markers
+    g_markers = label(P)
+    #Get the regions of the markers to access centroid later
+    PI = regionprops(g_markers)
+
+    #Create Watershed regions
+    Omega = watershed(util.invert(g),markers=g_markers)
+    #Create the adjacency graph of the labeled image
+    G = RAG(Omega,connectivity=2)
+
+    #Remove edges if centroids are to far away
+    for edge in G.edges():
+        if np.linalg.norm(np.array(PI[edge[0]-1].centroid) - np.array(PI[edge[1]-1].centroid)) > max_frag_dist:
+            G.remove_edge(edge[0],edge[1])
+
+    S = set()
+    #iterate over all conected components (cc)
+    for nodes_in_cc in tqdm(nx.algorithms.connectivity.edge_kcomponents.k_edge_subgraphs(G,1)):
+        #Set the subgraph of curent cc
+        H = G.subgraph(nodes_in_cc)
+        #Loop over the nodes of the subgraph
+        for v in nodes_in_cc:
+            #Add the isolated region of the curent node
+            if euler_number(Omega == v) == 1:
+                S.add(frozenset([v]))
+            #Loop over the distance from the curent node within the graph
+            for distance in range(1,max_search_depth+1):
+                #Get a dictonary containg the distance from curent node
+                distance_from_origin = nx.single_source_shortest_path_length(H, v,cutoff=distance)
+                #Loop over the amount of nodes
+                for node_amount in range(distance,len(distance_from_origin.values())):
+                    #Loop over all subsets of nodes that contain v and contain node_amount of nodes
+                    for node_subset in itertools.combinations([node for node in distance_from_origin.keys()], node_amount):
+                        node_subset = set([v]).union(set(node_subset))
+                        #Check other requirments and add the node set to S if they are fullfilled
+                        if frozenset(node_subset) not in S:
+                            if nx.is_connected(H.subgraph(node_subset)):
+                                #check that the merge of the regions is simply connected
+                                sub_img = np.zeros_like(Omega, dtype = bool)
+                                for region_label in node_subset:
+                                    sub_img += Omega == region_label
+                                if euler_number(sub_img) == 1:
+                                     S.add(frozenset(node_subset))
+
+    return Omega,S
